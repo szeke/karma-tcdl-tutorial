90a67c2f24cfe46c7f56457363122c8898cce9bb
szeke <szekely1401@gmail.com>
2015-04-22 23:24:43 -0700

    model authors

---
+++ datasets/comics_201405_csv/topics_general/topics_general-model.md
@@ -0,0 +1,24 @@
+## topics_general.csv
+
+### PyTransforms
+#### _topic1_
+From column: _Topic-general_
+>``` python
+return blTopic1(getValue("Topic-general"))
+```
+
+#### _topic2_
+From column: _Topic-general_
+>``` python
+return blTopic2(getValue("Topic-general"))
+```
+
+
+### Semantic Types
+| Column | Property | Class |
+|  ----- | -------- | ----- |
+
+
+### Links
+| From | Property | To |
+|  --- | -------- | ---|
--- karma/python/bl.py
+++ karma/python/bl.py
@@ -1,8 +1,33 @@
 
 
 def blAggregationUri(recordId):
-    return getTextHash(recordId)
+    #return getTextHash(recordId)
+    return recordId
+
 
 def blSourceResourceUri(recordId):
-    return getTextHash(recordId)+"/sourceResource"
+    return blAggregationUri(recordId)+"/sourceResource"
 
+
+def blAgentUri(lastFirst):
+    str = lastFirst.strip().replace('.','').lower()
+    list = str.split(',')
+    if len(list) == 2:
+    	str = list[1] + " " + list[0]
+	return "person/" + str.replace(' ','')
+
+
+def blCleanAgent(name):
+	return name.strip().replace('.','')
+
+
+def blTopicN(topic, index):
+	list = topic.split('/')
+	return list[index].strip()
+
+def blTopic1(topic):
+	return blTopicN(topic,0)
+
+
+def blTopic2(topic):
+	return blTopicN(topic,1)
